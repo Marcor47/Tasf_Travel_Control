50e4f366f0950ba4ae8ff5477842846c49e29629
Marcor <mark_rt0610@hotmail.com>
2026-04-22 15:26:03 -0500

    Carga inicial
    Java 21, spring boot

---
+++ TasfTravelControl/src/main/java/gruopo/_c/TasfTravelControl/TasfTravelControlApplication.java
@@ -0,0 +1,13 @@
+package gruopo._c.TasfTravelControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasfTravelControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasfTravelControlApplication.class, args);
+	}
+
+}
---
+++ TasfTravelControl/src/test/java/gruopo/_c/TasfTravelControl/TasfTravelControlApplicationTests.java
@@ -0,0 +1,13 @@
+package gruopo._c.TasfTravelControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasfTravelControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
